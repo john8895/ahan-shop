9157cc9d1f9c701b4389ed7ab6c8cefb5b16c0d7
John <paiching01@gmail.com>
2021-03-16 06:51:48 +0800

    first commit

--- routes/web.php
+++ routes/web.php
@@ -13,26 +13,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/about', function () {
-    return view('welcome');
-});
-Route::get('/news', function () {
-    return view('welcome');
-});
-Route::get('/products', function () {
-    return view('welcome');
-});
-Route::get('/contact', function () {
-    return view('welcome');
-});
-Route::get('/post/{id}/{name}', function ($id, $name) {
-    return "This post id is " . $id . " " . $name;
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+//Route::get('/about', function () {
+//    return view('welcome');
+//});
+//Route::get('/news', function () {
+//    return view('welcome');
+//});
+//Route::get('/products', function () {
+//    return view('welcome');
+//});
+//Route::get('/contact', function () {
+//    return view('welcome');
+//});
+//Route::get('/post/{id}/{name}', function ($id, $name) {
+//    return "This post id is " . $id . " " . $name;
+//});
+//
+//Route::get('admin/posts/examples', array('as'=>'admin.home', function(){
+//    $url = route('admin.home');
+//    return "This url is " . $url;
+//}));
 
-Route::get('admin/posts/examples', array('as'=>'admin.home', function(){
-    $url = route('admin.home');
-    return "This url is " . $url;
-}));
+//Route::get('/post/{id}', '\App\Http\Controllers\PostsController@index');
+
+//Route::get('resource', '\app\Http\Controllers\PostsController');
+
+Route::resource('post', '\App\Http\Controllers\PostsController');
